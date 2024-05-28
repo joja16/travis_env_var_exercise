147acf89cbc1970df7b4ddbd30a8262919d74a6f
joja16 <johnphamns@gmail.com>
2024-05-28 10:57:59 +0700

    Commit travis

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+// Set a local variable populated by the environment variable $NAME
+const FAVORITE_INSTRUCTOR = process.env.NAME;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  // while(true) {
-  //   console.log("Microservices rock!");
-  //   await sleep(5000);
-  // }
-
-  const favoriteFood = process.env.FAVORITE_FOOD;
-  console.log(`My favorite food is ${favoriteFood}`);
+  while(true) {
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
+    await sleep(5000);
+  }
 }
 
 main();
